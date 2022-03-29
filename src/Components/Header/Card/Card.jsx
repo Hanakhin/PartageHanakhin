9c16882f9e0ec0644e7a29dc0b1ad821d537a9db
Hanakhin <hanakhin.nounimassotte@gmail.com>
2022-03-29 18:39:25 +0200

    ajout des boutons

--- src/Components/Header/Card/Card.jsx
+++ src/Components/Header/Card/Card.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from "react";
 import css from './Card.css'
-import spider from '../Card/spider.jpg';
-import justice from '../Card/justice.jpg';
-import guardian from '../Card/guardian.jpg';
+import spider from '../Card/img/spider.jpg';
+import justice from '../Card/img/justice.jpg';
+import guardian from '../Card/img/guardian.jpg';
+import iconbackward from '../Card/img/iconbackward.png';
+import iconforward from '../Card/img/iconforward.png';
 
-export function DisplayCards() {
-
-   // let [image, setImage] = useState(justice);
+export function DisplayCards(props) {
 
     const image = [guardian,justice,spider];
-
     const [index, setIndex] = useState(0);
+
 
     return (
         <div className="Card">
 
             <img src={image[index]} />
             <div className="boutton">
-                <button onClick={() => setIndex(Math.max(index-1,0))}>Image Précédente</button>
-                <button onClick={() => setIndex(Math.min(index+1,image.length-1))}>Image Suivante</button>
-                
+                <img className="icons" src={iconbackward} onClick={() => setIndex(Math.max(index-1,0))}></img> 
+                <img className="icons" src={iconforward} onClick={() => setIndex(Math.min(index+1,image.length-1))}></img>
             </div>
         </div>
     )
